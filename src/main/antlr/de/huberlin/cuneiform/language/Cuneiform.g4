75cd23c36a5a8480d1fc1c2d3c432e4780ee72e2
Jörgen Brandt <joergen.brandt@onlinehome.de>
2014-04-30 19:08:57 +0200

    Maven project.

---
+++ src/main/antlr/de/huberlin/cuneiform/language/Cuneiform.g4
@@ -0,0 +1,212 @@
+/*******************************************************************************
+ * In the Hi-WAY project we propose a novel approach of executing scientific
+ * workflows processing Big Data, as found in NGS applications, on distributed
+ * computational infrastructures. The Hi-WAY software stack comprises the func-
+ * tional workflow language Cuneiform as well as the Hi-WAY ApplicationMaster
+ * for Apache Hadoop 2.x (YARN).
+ *
+ * List of Contributors:
+ *
+ * Jörgen Brandt (HU Berlin)
+ * Marc Bux (HU Berlin)
+ * Ulf Leser (HU Berlin)
+ *
+ * Jörgen Brandt is funded by the European Commission through the BiobankCloud
+ * project. Marc Bux is funded by the Deutsche Forschungsgemeinschaft through
+ * research training group SOAMED (GRK 1651).
+ *
+ * Copyright 2014 Humboldt-Universität zu Berlin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+grammar Cuneiform ;
+
+options { superClass = BaseCuneiformParser; }
+
+@parser::header {
+import java.util.Set;
+import java.util.HashSet;
+import java.util.List;
+import java.util.LinkedList;
+}
+
+// PARSER RULES
+
+// top level definition
+
+script     : toplevel* EOF             { checkPost(); } ;
+
+toplevel   : importFile | declare | label | deftask | extend | assign | target
+           | defmacro ;
+
+// expressions
+
+importFile : ( IMPORT|INCLUDE )id=ID   { importFile( $id ); }
+             SEMICOLON ;
+
+declare    : DECLARE id=ID SEMICOLON   { setDeclare( $id ); } ;
+
+label      : LABEL outer=ID            { addLabel( $outer ); }
+             ( COLON( inner=ID         { addLabelMember( $outer, $inner ); }
+             )+ )? SEMICOLON ;
+
+deftask    : DEFTASK n=ID              { addDefTask( $n ); }
+             ( IN( i=ID                { addDefTaskLabel( $n, $i ); }
+             )+ )?                     { verifyDefTaskLabel( $n ); }
+             LPAREN( vout=outvar       { addDefTaskOutput( $n, $vout.v ); }
+             )* COLON( pin=param       { addDefTaskParam( $n, $pin.p ); }
+             )* RPAREN b=BODY          { setDefTaskBody( $n, $b );
+                                         createMacroFromDefTask( $n ); } ;
+
+param returns [DefTaskParam p]
+           : s=singleParam             { $p = $s.p; }
+           | r=reduceParam             { $p = $r.p; }
+           | c=correlParam             { $p = $c.p; } ;
+
+singleParam returns [CorrelParam p]
+           :                           { int type = 0; }
+             ( TILDE                   { type = 1; }
+             )? i=ID                   { $p = new CorrelParam( $i, type ); } ;
+
+reduceParam returns [ReduceParam p]
+           :                           { int type = 0; }
+             ( TILDE                   { type = 1; }
+             )? LTAG i=ID RTAG         { $p = new ReduceParam( $i, type ); } ;
+
+correlParam returns [CorrelParam p]
+           : LSQUAREBR                 { $p = new CorrelParam(); int type; }
+             (                         { type = 0; }
+             ( TILDE                   { type = 1; }
+             )? k=ID                   { $p.addParam( $k, type ); }
+             )+ RSQUAREBR ;
+             
+outvar returns [DefTaskOutput v]
+           :                           { int type = 0; }
+             ( QUOTE                   { type = 2; }
+             | TILDE                   { type = 1; }
+             )?( LTAG i=ID RTAG        { $v = new ReduceOutput( $i, type ); }
+             | j=ID                    { $v = new SingleOutput( $j, type ); }
+             ) ;
+
+extend     : EXTEND                    { addExtend(); }
+             ( id=ID                   { addExtendLabel( $id ); }
+             )+( b=before              { addAuxMethod( $b.am ); }
+             | a=after                 { addAuxMethod( $a.am ); }
+             | fi=forinput             { addAuxMethod( $fi.am ); }
+             | fo=foroutput            { addAuxMethod( $fo.am ); }
+             )+ ;
+
+before returns [BeforeMethod am]
+           : f=BEFORE                  { $am = new BeforeMethod( $f.getLine() ); }
+             ( LPAREN COLON      
+             ( id=ID                   { $am.addVar( $id ); }
+             )* RPAREN )? b=BODY       { $am.setBody( $b ); } ;
+
+after returns [AfterMethod am]
+           : f=AFTER                   { $am = new AfterMethod( $f.getLine() ); }
+             ( LPAREN              
+             ( id=ID                   { $am.addVar( $id ); }
+             )* COLON RPAREN )? b=BODY { $am.setBody( $b ); } ;
+
+forinput returns [ForInputMethod am]
+           : FOR INPUT id=ID           { $am = new ForInputMethod( $id ); }
+             b=BODY                    { $am.setBody( $b ); } ;
+
+foroutput returns [ForOutputMethod am]
+           : FOR OUTPUT id=ID          { $am = new ForOutputMethod( $id ); }
+             b=BODY                    { $am.setBody( $b ); } ;
+
+assign     :                           { addAssign(); }
+             ( i=ID                    { addAssignVar( $i ); }
+             )+ EQ e=expression        { addAssignExpression( $e.r ); }
+             SEMICOLON ;
+
+expression returns [List<Expression> r]
+           :                           { $r = new LinkedList<>(); }
+             ( m=macroexpr             { $r.add( $m.r ); }
+             | id=ID                   { $r.add( new IdExpression( $id ) ); }
+             | a=applyexpr             { $r.add( $a.r ); }
+             | s=stringexpr            { $r.add( $s.r ); }
+             )+ ;
+             
+macroexpr returns [MacroExpression r]
+           : i=ID LPAREN               { $r = new MacroExpression( $i ); }
+             ( p=ID COLON e=expression { $r.addParam( $p, $e.r ); }
+             )* RPAREN ;
+
+applyexpr returns [ApplyExpression r]
+           : APPLY LPAREN              { $r = new ApplyExpression(); }
+             ( p=ID COLON e=expression { $r.addParam( $p, $e.r ); }
+             )* RPAREN ;
+
+stringexpr returns [StringExpression r]
+           :                           { boolean stage = true; }
+             ( TILDE                   { stage = false; }
+             )?( s=STRING1             { $r = new StringExpression( $s, stage ); }
+             | s=STRING2               { $r = new StringExpression( $s, stage ); } ) ;
+
+target     : TARGET                    { addTarget(); }
+             ( id=ID                   { addTarget( $id ); }
+             )+ SEMICOLON ;
+
+defmacro   : DEFMACRO id=ID LPAREN     { addDefMacro( $id ); }
+             ( v=ID                    { addDefMacroVar( $v ); }
+             )* RPAREN e=expression    { setDefMacroExpression( $e.r ); }
+             SEMICOLON ;
+               
+// LEXER RULES
+
+// comments
+
+COMMENT1   : ( '#'|'//'|'%' )~'\n'*    { skip(); };
+COMMENT2   : '/*' .*? '*/'             { skip(); };
+
+// keywords
+
+AFTER      : 'after' ;
+APPLY      : 'apply' ;
+BEFORE     : 'before' ;
+COLON      : ':' ;
+DECLARE    : 'declare' ;
+DEFMACRO   : 'defmacro' ;
+DEFTASK    : 'deftask' ;
+EQ         : '=' ;
+EXTEND     : 'extend' ;
+FOR        : 'for' ;
+IMPORT     : 'import' ;
+IN         : 'in' ;
+INCLUDE    : 'include' ;
+INPUT      : 'input' ;
+LABEL      : 'label' ;
+LPAREN     : '(' ;
+LSQUAREBR  : '[' ;
+LTAG       : '<' ;
+OUTPUT     : 'output' ;
+PREFIX     : 'prefix' ;
+QUOTE      : '`' ;
+TARGET     : 'target' ;
+RPAREN     : ')' ;
+RSQUAREBR  : ']' ;
+RTAG       : '>' ;
+SEMICOLON  : ';' ;
+TILDE      : '~' ;
+
+// non-keywords
+
+BODY       : '*{' .*? '}*' ;
+STRING1    : '\'' .*? '\'' ;
+STRING2    : '"' .*? '"' ;
+ID         : [a-zA-Z0-9/\.\-_\+\*]+ ;
+WS         : [ ,\n\t\r]                { skip(); } ;
+ILLEGAL    : . ;
---
+++ src/main/java/de/huberlin/cuneiform/compiler/local/LocalDispatcher.java
@@ -0,0 +1,342 @@
+/*******************************************************************************
+ * In the Hi-WAY project we propose a novel approach of executing scientific
+ * workflows processing Big Data, as found in NGS applications, on distributed
+ * computational infrastructures. The Hi-WAY software stack comprises the func-
+ * tional workflow language Cuneiform as well as the Hi-WAY ApplicationMaster
+ * for Apache Hadoop 2.x (YARN).
+ *
+ * List of Contributors:
+ *
+ * Jörgen Brandt (HU Berlin)
+ * Marc Bux (HU Berlin)
+ * Ulf Leser (HU Berlin)
+ *
+ * Jörgen Brandt is funded by the European Commission through the BiobankCloud
+ * project. Marc Bux is funded by the Deutsche Forschungsgemeinschaft through
+ * research training group SOAMED (GRK 1651).
+ *
+ * Copyright 2014 Humboldt-Universität zu Berlin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package de.huberlin.cuneiform.compiler.local;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringReader;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.json.JSONException;
+
+import de.huberlin.cuneiform.dag.ExecDag;
+import de.huberlin.cuneiform.dag.Invocation;
+import de.huberlin.cuneiform.dag.JsonReportEntry;
+import de.huberlin.cuneiform.dag.NotDerivableException;
+
+public class LocalDispatcher extends ExecDag {
+	
+	private File buildDir;
+	private File logFile;
+	
+	public static final String SCRIPT_FILENAME = "__script__";
+	public static final String SUCCESS_FILENAME = "__success__";
+	// private static final int NTHREADS = 4;
+	
+	public LocalDispatcher( File buildDir ) {
+		setBuildDir( buildDir );
+	}
+
+	public LocalDispatcher( File buildDir, File logFile ) {
+		setBuildDir( buildDir );		
+		setLogFile( logFile );
+	}
+
+	public LocalDispatcher( File buildDir, File logFile, String dagid ) {
+		super( dagid );
+		setBuildDir( buildDir );
+		setLogFile( logFile );
+	}
+	
+	public File getLogFile() {
+		
+		if( logFile == null )
+			return new File( buildDir.getAbsolutePath()+"/log_"+getDagId()+".txt" );
+		
+		return logFile;
+	}
+	
+	public void run()
+	throws IOException, InterruptedException, NotDerivableException, JSONException {
+		
+		Set<Invocation> invocationSet;
+		if( buildDir == null )
+			throw new NullPointerException( "Build directory must not be null." );
+		
+		if( !buildDir.exists() )
+			if( !buildDir.mkdirs() )
+				throw new IOException(
+					"Unable to create sandbox folder '"
+					+buildDir.getAbsolutePath()+"'." );
+		
+		do {
+			
+			invocationSet = getReadyInvocationSet();
+			
+			for( Invocation invoc : invocationSet )				
+				evalReport( dispatch( invoc ) );
+			
+		} while( !invocationSet.isEmpty() );		
+	}
+	
+	public void setBuildDir( File buildDir ) {
+		
+		if( buildDir == null )
+			throw new NullPointerException( "Build directory must not be null." );
+		
+		this.buildDir = buildDir;
+	}
+	
+	public void setLogFile( File logFile ) {
+		this.logFile = logFile;
+	}
+	
+	protected Set<JsonReportEntry> dispatch( Invocation invocation )
+	throws IOException, InterruptedException, NotDerivableException, JSONException {
+		
+		File scriptFile;
+		Process process;
+		int exitValue;
+		Set<JsonReportEntry> report;
+		String line;
+		String[] arg;
+		String value;
+		int i;
+		StringBuffer buf;
+		File location;
+		File reportFile;
+		StreamConsumer stdoutConsumer, errConsumer;
+		ExecutorService executor;
+		String signature;
+		Path srcPath, destPath;
+		File successMarker;
+		
+		if( invocation == null )
+			throw new NullPointerException( "Invocation must not be null." );
+		
+		if( !invocation.isReady() )
+			throw new RuntimeException( "Cannot dispatch invocation that is not ready." );
+		
+		location = new File( buildDir.getAbsolutePath()+"/"+invocation.getSignature() );
+		successMarker = new File( location.getAbsolutePath()+"/"+SUCCESS_FILENAME );
+		reportFile = new File( location.getAbsolutePath()+"/"+Invocation.REPORT_FILENAME );
+		
+		if( !successMarker.exists() ) {
+			
+			if( location.exists() )
+				FileUtils.deleteDirectory( location );
+		
+			if( !location.mkdirs() )
+				throw new IOException( "Could not create invocation location." );
+			
+			scriptFile = new File( location.getAbsolutePath()+"/"+SCRIPT_FILENAME );
+			
+			try( BufferedWriter writer = new BufferedWriter( new FileWriter( scriptFile, false ) ) ) {
+				
+				// write away script
+				writer.write( invocation.toScript() );
+				
+				
+			}
+			
+			scriptFile.setExecutable( true );
+			
+			for( String filename : invocation.getStageInList() ) {
+				
+				
+
+				if( filename.charAt( 0 ) != '/' && filename.indexOf( '_' ) >= 0 ) {
+
+					signature = filename.substring( 0, filename.indexOf( '_' ) );
+					
+					srcPath = FileSystems.getDefault().getPath( buildDir.getAbsolutePath()+"/"+signature+"/"+filename );				
+					destPath = FileSystems.getDefault().getPath( buildDir.getAbsolutePath()+"/"+invocation.getSignature()+"/"+filename );
+					Files.createSymbolicLink( destPath, srcPath );
+				}
+			}
+	
+			
+			arg = new String[] {
+					"/usr/bin/time",
+					"-a",
+					"-o",
+					location.getAbsolutePath()+"/"+Invocation.REPORT_FILENAME,
+					"-f",
+					"{"
+					+JsonReportEntry.ATT_TIMESTAMP+":"+System.currentTimeMillis()+","
+					+JsonReportEntry.ATT_RUNID+":\""+invocation.getDagId()+"\","
+					+JsonReportEntry.ATT_TASKID+":"+invocation.getTaskNodeId()+","
+					+JsonReportEntry.ATT_TASKNAME+":\""+invocation.getTaskName()+"\","
+					+JsonReportEntry.ATT_LANG+":\""+invocation.getLangLabel()+"\","
+					+JsonReportEntry.ATT_INVOCID+":"+invocation.getSignature()+","
+					+JsonReportEntry.ATT_KEY+":\""+JsonReportEntry.KEY_INVOC_TIME+"\","
+					+JsonReportEntry.ATT_VALUE+":"
+					+"{\"realTime\":%e,\"userTime\":%U,\"sysTime\":%S,"
+					+"\"maxResidentSetSize\":%M,\"avgResidentSetSize\":%t,"
+					+"\"avgDataSize\":%D,\"avgStackSize\":%p,\"avgTextSize\":%X,"
+					+"\"nMajPageFault\":%F,\"nMinPageFault\":%R,"
+					+"\"nSwapOutMainMem\":%W,\"nForcedContextSwitch\":%c,"
+					+"\"nWaitContextSwitch\":%w,\"nIoRead\":%I,\"nIoWrite\":%O,"
+					+"\"nSocketRead\":%r,\"nSocketWrite\":%s,\"nSignal\":%k}}",
+					scriptFile.getAbsolutePath() };
+			
+			
+			
+			// run script
+			process = Runtime.getRuntime().exec( arg, null, location );
+			
+			executor = Executors.newCachedThreadPool();
+			
+			stdoutConsumer = new StreamConsumer( process.getInputStream() );
+			executor.execute( stdoutConsumer );
+			
+			errConsumer = new StreamConsumer( process.getErrorStream() );
+			executor.execute( errConsumer );
+			
+			executor.shutdown();
+			
+			exitValue = process.waitFor();
+			if( !executor.awaitTermination( 4, TimeUnit.SECONDS ) )
+				throw new RuntimeException(
+					"Consumer threads did not finish orderly." );
+			
+			
+			try( BufferedWriter reportWriter = new BufferedWriter( new FileWriter( reportFile, true ) ) ) {
+	
+			
+				if( exitValue != 0 ) {
+					
+					System.err.println( "[script]" );
+					
+					try( BufferedReader reader = new BufferedReader( new StringReader( invocation.toScript() ) ) ) {
+						
+						i = 0;
+						while( ( line = reader.readLine() ) != null )
+							System.err.println( String.format( "%02d  %s", ++i, line ) );
+					}
+					
+					System.err.println( "[out]" );
+					try( BufferedReader reader = new BufferedReader( new StringReader( stdoutConsumer.getContent() ) ) ) {
+						
+						while( ( line = reader.readLine() ) != null )
+							System.err.println( line );
+					}
+					
+					System.err.println( "[err]" );
+					try( BufferedReader reader = new BufferedReader( new StringReader( errConsumer.getContent() ) ) ) {
+						
+						while( ( line = reader.readLine() ) != null )
+							System.err.println( line );
+					}
+					
+					System.err.println( "[end]" );
+					
+					throw new RuntimeException(
+						"Invocation of task '"+invocation.getTaskName()
+						+"' with signature "+invocation.getSignature()
+						+" terminated with non-zero exit value. Exit value was "
+						+exitValue+"." );
+				}
+				
+				
+				try( BufferedReader reader = new BufferedReader( new StringReader( stdoutConsumer.getContent() ) ) ) {
+					
+					buf = new StringBuffer();
+					while( ( line = reader.readLine() ) != null )
+						buf.append( line.replaceAll( "\\\\", "\\\\\\\\" ).replaceAll( "\"", "\\\"" ) ).append( '\n' );
+					
+					
+					value = buf.toString();
+					if( !value.isEmpty() )
+					
+					reportWriter.write( new JsonReportEntry( invocation, JsonReportEntry.KEY_INVOC_STDOUT, value ).toString() );
+				}
+				try( BufferedReader reader = new BufferedReader( new StringReader( errConsumer.getContent() ) ) ) {
+					
+					buf = new StringBuffer();
+					while( ( line = reader.readLine() ) != null )
+						buf.append( line.replaceAll( "\\\\", "\\\\\\\\" ).replaceAll( "\"", "\\\"" ) ).append( '\n' );
+					
+					value = buf.toString();
+					if( !value.isEmpty() )
+					
+					reportWriter.write( new JsonReportEntry( invocation, JsonReportEntry.KEY_INVOC_STDERR, value ).toString() );
+				}
+		
+			}			
+		}
+		
+		// gather report
+		report = new HashSet<>();
+		try(
+			BufferedReader reader =
+				new BufferedReader( new FileReader( reportFile ) ) ) {
+			
+			while( ( line = reader.readLine() ) != null ) {
+				
+				line = line.trim();
+				
+				if( line.isEmpty() )
+					continue;
+				
+				report.add( new JsonReportEntry( line ) );
+			}
+			
+		}
+		
+		invocation.evalReport( report );
+
+		if( !successMarker.exists() )
+			if( !successMarker.createNewFile() )
+				throw new IOException( "Could not create success marker." );
+		
+		return report; 
+	}
+	
+	protected void evalReport( Set<JsonReportEntry> report ) throws IOException {
+		
+		try( BufferedWriter writer = new BufferedWriter( new FileWriter( getLogFile(), true ) ) ) {
+			
+			for( JsonReportEntry entry : report ) {
+				writer.write( entry.toString() );
+				writer.write( '\n' );
+				System.out.println( entry );
+			}
+
+		}
+	}
+	
+
+}
